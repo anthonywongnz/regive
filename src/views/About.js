6e97d539dad57f364fe4b062860fcf449eb7869a
minh <Lminhphu0304@gmail.com>
2020-07-26 13:53:42 +1200

    Add content for about us page

--- src/views/About.js
+++ src/views/About.js
@@ -3,8 +3,58 @@ import React from "react";
 function About() {
   return (
     <div>
-      <h1 className="font-bold text-2xl mb-3">This is the about page</h1>
-      <p>This is some random content about us.</p>
+      <h1 className="font-bold text-2xl mb-3">About Regive</h1>
+      <h4 className="text-l mt-5 mb-5">Reuse, reduce, recycle – Regive.</h4>
+      <p className="mb-3 mt-3">
+        Regive is a service where someone can list things to give away as donations and another person who may be seeking donated things can request for it.
+      </p>
+      <p className="mb-3 mt-3">
+        While Regive is new for adoption, it is built to be simple enough with listing items separately or together as a bundle for donation and once signed up on to Regive, it can be viewed and requested by others on the service.
+      </p>
+      <p className="mb-3 mt-3">
+        We understand that community cooperation is built on trust and transparency. Therefore, the goal is to provide a service that serves as the basis for people to dump less out on the streets and instead list on Regive as “Givers” and other people “Receivers” can find to request for the donated things. Also in the future we intend to include donation centers as a bridge communication for “Givers” to communicate with donation centers that can accept their donations.
+      </p>
+      <h4 className="font-bold text-l mt-5 mb-5">
+        Why is Regive built and how is it free?
+      </h4>
+      <p className="mb-3 mt-3">
+        We made Regive free because we intend to build trust and security – especially during these unforeseen times where a global pandemic of COVID-19 has caused so much disruption and with some people needing certain things or having to relocate. It is difficult for the people that had to quickly depart out of a country before their country borders closed off and with having a lot of things to get rid of, rather than dumping it on the sides of the road or overloading clothing bins, this was our motivation to build a first version of Regive that can help some people who are living out of a suitcase and doesn’t want to spend too much on more things and some people who want to giveaway things because they can’t take it with them. Regive was created for the IBM Call for Code 2020 Community Cooperation challenge (or “Hackathon”).
+      </p>
+      <p className="mb-3 mt-3">
+        The team behind Regive built it because we have experienced similar situations of being overseas in a different country for a short period of time and were able to accumulate a lot of things that we try to get rid of before returning back to our home country. Therefore we intend to have a platform that helps people cooperate, trust and support one another.
+      </p>
+      <h4 className="font-bold text-l mt-5 mb-5">
+        Who is the team behind Regive?
+      </h4>
+      <p className="mb-3 mt-3">
+        The team behind Regive are not essential workers, and we’re not on the front line workers - but we’re techies, and we accept challenges to tackle on to build for.
+      </p>
+      <p className="mb-10 mt-10">
+        Anthony Wong
+      </p>
+      <p className="mb-10 mt-10">
+        Minh Phu
+      </p>
+      <p className="mb-10 mt-10">
+        Tash Bett
+      </p>
+      <p className="mb-10 mt-10">
+        Nicole Yue-Lin
+      </p>
+      <p className="mb-10 mt-10">
+        Fiona Lai
+      </p>
+      <h4 className="font-bold mt-5 mb-5">
+        Contributors
+      </h4>
+      <p className="mb-3 mt-3">
+        In a time where we are living in a crisis on top of a crisis, we couldn’t have shaped Regive to become what it is without extra help and contribution from others!
+      </p>
+      <ul className="list-inside list-disc">
+        <li>Walter Lim</li>
+        <li>Guillermo Ramírez-Prado</li>
+        <li>Clancy Wall</li>
+      </ul>
     </div>
   );
 }
